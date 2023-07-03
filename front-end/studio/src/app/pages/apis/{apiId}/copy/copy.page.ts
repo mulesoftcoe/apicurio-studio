93ca96de99fabd645d21b06d0127bdc7d4342fa0
nitinprakash05 <nitinprakash05@gmail.com>
2023-07-03 17:30:34 +0530

    title modified and apicurio name changed to boilerplate
    title modified and apicurio name changed to boilerplate

--- front-end/studio/src/app/pages/apis/{apiId}/copy/copy.page.ts
+++ front-end/studio/src/app/pages/apis/{apiId}/copy/copy.page.ts
@@ -60,9 +60,9 @@ export class CopyPageComponent extends AbstractPageComponent {
      */
     protected pageTitle(): string {
         if (this.api.name) {
-            return "Apicurio Studio - Copy API :: " + this.api.name;
+            return "Boilerplate - Copy API :: " + this.api.name;
         } else {
-            return "Apicurio Studio - Copy API";
+            return "Boilerplate - Copy API";
         }
     }
 
